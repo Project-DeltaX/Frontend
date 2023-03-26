84fe81419b79d24bbd04fba2dfdb500096aaa2a3
Tharanika11 <perinparasat.20@uom.lk>
2023-03-26 23:51:11 +0530

    availability complete

---
+++ src/pages/InterviewSchedule/PanelAvailability.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+  
+function createData(PanelMemberName, JobTitle, Email, Status) {
+    return { PanelMemberName, JobTitle, Email, Status };
+  }
+const PanelAvailability = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(
+              "https://lcugzprcu3.execute-api.us-east-1.amazonaws.com/dev/p-availability-data"
+            );
+            const json = await response.json();
+            setData(json);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        fetchData();
+      }, []);
+
+return(
+    <div>
+    <h3> Panel Member</h3>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+       
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>PanelMemberName</StyledTableCell>
+            <StyledTableCell align="right">JobTitle</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">Status</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.PanelMemberName}
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                {row.JobTitle}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.email}</StyledTableCell>
+              <StyledTableCell align="right">
+                {row.Status}
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </div>
+
+);
+};
+
+export default PanelAvailability;
+
