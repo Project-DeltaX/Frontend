4a5e894490de92f4cc216dd9230375cbc1c88548
Tharanika11 <perinparasat.20@uom.lk>
2023-06-14 03:28:36 +0530

    mailing added

--- src/pages/InterviewSchedule/test.js
+++ src/pages/InterviewSchedule/test.js
@@ -1,206 +1,275 @@
-// import React, { useState, useEffect } from "react";
-// import Table from "@mui/material/Table";
-// import TableBody from "@mui/material/TableBody";
-// import TableCell from "@mui/material/TableCell";
-// import TableContainer from "@mui/material/TableContainer";
-// import TableHead from "@mui/material/TableHead";
-// import TableRow from "@mui/material/TableRow";
-// import Paper from "@mui/material/Paper";
-// import Grid from "@mui/material/Grid";
-// import FormControl from "@mui/material/FormControl";
-// import Checkbox from "@mui/material/Checkbox";
-// import Button from "@mui/material/Button";
-// import SendIcon from "@mui/icons-material/Send";
-// import Stack from "@mui/material/Stack";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  Checkbox,
+  Stack,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { TextField, Grid } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { Dayjs } from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
-// const SMCandidate = () => {
-//   const [sData, setSData] = useState([]);
-//   const [selectedCandidates, setSelectedCandidates] = useState([]);
+const Schedule = () => {
+  const [sData, setSData] = useState([]);
+  const [selectedCandidates, setSelectedCandidates] = useState([]);
+  const [mode, setMode] = useState("");
+  const [value, setValue] = useState("");
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(
-//           "https://71sozhk0d6.execute-api.us-east-1.amazonaws.com/st1/internlist"
-//         );
-//         const jsonData = await response.json();
-//         setSData(jsonData);
-//         console.log(sData);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://nxhk699z3m.execute-api.us-east-1.amazonaws.com/stage1/schedule"
+        );
+        const jsonData = await response.json();
+        setSData(jsonData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-//   const handleCheckboxChange = (event, candidate) => {
-//     if (event.target.checked) {
-//       setSelectedCandidates([...selectedCandidates, candidate]);
-//     } else {
-//       setSelectedCandidates(
-//         selectedCandidates.filter((c) => c.candidateEmail !== candidate.candidateEmail)
-//       );
-//     }
-//   };
-
-//   const handleSendEmail = async () => {
-//     try {
-//       const recipients = selectedCandidates.map((candidate) => candidate.candidateEmail);
-
-//       const response = await axios.post(
-//         "https://9ippym1vkf.execute-api.us-east-1.amazonaws.com/new1/internmail", // Replace with your Lambda function endpoint
-//         { recipients }
-//       );
-
-//       console.log(response.data);
-//     } catch (error) {
-//       console.error("Error:", error);
-//     }
-//   };
-
-//   return (
-//     <Grid container spacing={2}>
-//       <Grid item xs={12}>
-//         <FormControl
-//           fullWidth
-//           sx={{
-//             "& label": {
-//               color: "#1e0342", // change text color
-//               fontWeight: "bold",
-//             },
-//           }}
-//         ></FormControl>
-
-//         <Grid item xs={12}>
-//           <div className="Table">
-//             <TableContainer
-//               component={Paper}
-//               style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
-//             >
-//               <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//                 <TableHead sx={{ backgroundColor: "#9485a8" }}>
-//                   <TableRow>
-//                     <TableCell />
-//                     <TableCell>CandidateName</TableCell>
-//                     <TableCell align="left">JobRole</TableCell>
-//                     <TableCell align="left">CandidateEmail</TableCell>
-//                     <TableCell align="left">Status</TableCell>
-//                   </TableRow>
-//                 </TableHead>
-//                 <TableBody style={{ color: "Bluet" }}>
-//                   {sData.map((row) => (
-//                     <TableRow
-//                       style={{ backgroundColor: "#b8a9cc" }}
-//                       key={row.name}
-//                       sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-//                     >
-//                       <TableCell padding="checkbox">
-//                         <Checkbox
-//                           checked={selectedCandidates.includes(row)}
-//                           onChange={(event) => handleCheckboxChange(event, row)}
-//                         />
-//                       </TableCell>
-//                       <TableCell component="th"
-// scope="row">
-// {row.candidateName}
-// </TableCell>
-// <TableCell align="left">{row.jobRole}</TableCell>
-// <TableCell align="left">{row.candidateEmail}</TableCell>
-// <TableCell align="left">{row.status}</TableCell>
-// </TableRow>
-// ))}
-// </TableBody>
-// </Table>
-// </TableContainer>
-// </div>
-// </Grid>
-// <Grid item xs={12}>
-// <Stack direction="row" spacing={2}>
-// <Button
-// variant="contained"
-// endIcon={<SendIcon />}
-// style={{ backgroundColor: "#1e0342" }}
-// onClick={handleSendEmail}
-// disabled={selectedCandidates.length === 0}
-// >
-// Send Email
-// </Button>
-// </Stack>
-// </Grid>
-// </Grid>
-// </Grid>
-// );
-// };
-
-// export default SMCandidate;
-
-const [recipientEmail, setRecipientEmail] = useState("");
-const [emailSubject, setEmailSubject] = useState("");
-const [emailBody, setEmailBody] = useState("");
-
-const handleSendEmail = async (event) => {
-  event.preventDefault();
-
-  
-
-  const ses = new AWS.SES({ apiVersion: "2010-12-01" });
-
-  const emailParams = {
-    Source: "perinparasat.20@uom.lk", // Update with your SES verified email address
-    Destination: {
-      ToAddresses: [recipientEmail],
-    },
-    Message: {
-      Subject: {
-        Data: emailSubject,
-      },
-      Body: {
-        Text: {
-          Data: emailBody,
-        },
-      },
-    },
+  const handleModeChange = (event) => {
+    setMode(event.target.value);
   };
 
-  try {
-    const data = await ses.sendEmail(emailParams).promise();
-    console.log("Email sent successfully", data);
-    // Add any success message or further actions here
-  } catch (error) {
-    console.error("Error sending email", error);
-    // Add error handling or display error message here
-  }
+  const handleCheckboxChange = (event, candidateEmail) => {
+    const checked = event.target.checked;
+
+    if (checked) {
+      setSelectedCandidates([candidateEmail]);
+    } else {
+      setSelectedCandidates([]);
+    }
+  };
+
+  const handleEditButtonClick = async () => {
+    // Update the selected candidate's interview time and venue
+    const candidateEmail = selectedCandidates[0];
+    const candidate = sData.find((item) => item.candidateEmail === candidateEmail);
+    const updatedCandidate = { ...candidate, interviewTime: value, venue: mode };
+
+    try {
+      const response = await fetch(
+        "https://nxhk699z3m.execute-api.us-east-1.amazonaws.com/stage1/schedule",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedCandidate),
+        }
+      );
+
+      if (response.ok) {
+        // Update the state with the updated candidate
+        const updatedData = sData.map((item) =>
+          item.candidateEmail === candidateEmail ? updatedCandidate : item
+        );
+        setSData(updatedData);
+        setSelectedCandidates([]);
+        setValue("");
+        setMode("");
+      } else {
+        console.error("Failed to update candidate data");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteButtonClick = () => {
+    // Handle delete logic for selected candidates
+    console.log("Delete Button Clicked");
+  };
+
+  const getStatusColor = (status) => {
+    if (status === "finalized") {
+      return "green";
+    }
+    return "red";
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} spacing={4}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <TimePicker
+            label="Basic example"
+            value={value}
+            onChange={(newValue) => {
+              // Validate the hour and minute values
+              const hour = newValue?.hour();
+              const minute = newValue?.minute();
+              if (hour >= 0 && hour <= 12 && minute >= 0 && minute <= 60) {
+                setValue(newValue);
+              } else {
+                // Handle invalid time
+                console.log("Invalid time selected");
+              }
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      </Grid>
+
+      <Grid item xs={8}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Interview Mode</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={mode}
+            label="Interview Mode"
+            onChange={handleModeChange}
+          >
+            <MenuItem value="On site">On site-1st floor</MenuItem>
+            <MenuItem value="Online">Online</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs={12} spacing={4}>
+        <TableContainer
+          component={Paper}
+          style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead sx={{ backgroundColor: "#9485a8" }}>
+              <TableRow>
+                <TableCell align="left">Select</TableCell>
+                <TableCell align="left">Candidate</TableCell>
+                <TableCell align="left">PanelMember</TableCell>
+                <TableCell align="left">InterviewDate</TableCell>
+                <TableCell align="left">EmailPanel</TableCell>
+                <TableCell align="left">CandidateEmail</TableCell>
+                <TableCell align="left">Venue</TableCell>
+                <TableCell align="left">Time</TableCell>
+                <TableCell align="left">Status</TableCell>
+                <TableCell align="left">Edit/Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody style={{ color: "white" }}>
+              {sData.map((row) => (
+                <TableRow
+                  style={{ backgroundColor: "#b8a9cc" }}
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="left">
+                    <Checkbox
+                      checked={selectedCandidates.includes(row.candidateEmail)}
+                      onChange={(event) =>
+                        handleCheckboxChange(event, row.candidateEmail)
+                      }
+                    />
+                  </TableCell>
+                  <TableCell align="left">{row.candidateName}</TableCell>
+                  <TableCell align="left">{row.panelMemberName}</TableCell>
+                  <TableCell align="left">{row.date}</TableCell>
+                  <TableCell align="left">{row.panelMemberEmail}</TableCell>
+                  <TableCell align="left">{row.candidateEmail}</TableCell>
+                  <TableCell align="left">{row.Venue}</TableCell>
+                  <TableCell align="left">{row.Time}</TableCell>
+                  <TableCell align="left">
+                    <Button
+                      variant="outlined"
+                      style={{
+                        backgroundColor: getStatusColor(row.status),
+                        color: "white",
+                      }}
+                      disabled={row.status === "finalized"}
+                      onClick={() => {
+                        if (row.status !== "finalized") {
+                          // Update the status to "finalized" when clicked
+                          const updatedData = sData.map((item) =>
+                            item === row
+                              ? { ...item, status: "finalized" }
+                              : item
+                          );
+                          setSData(updatedData);
+                        }
+                      }}
+                    >
+                      {row.status}
+                    </Button>
+                  </TableCell>
+                  <TableCell align="left
+                    <Stack direction="row" spacing={1}>
+                      <Button
+                        variant="contained"
+                        startIcon={<EditIcon />}
+                        style={{
+                          backgroundColor: "#1e0342",
+                          color: "white",
+                        }}
+                        onClick={() => {
+                          // Handle edit logic for the current candidate
+                          handleEditButtonClick(row.candidateEmail);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        startIcon={<DeleteIcon />}
+                        style={{
+                          backgroundColor: "#1e0342",
+                          color: "white",
+                        }}
+                        onClick={() => {
+                          // Handle delete logic for the current candidate
+                          handleDeleteButtonClick(row.candidateEmail);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item xs={12} spacing={4}>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="outlined"
+            startIcon={<DeleteIcon />}
+            style={{ backgroundColor: "#1e0342", color: "white" }}
+            onClick={handleDeleteButtonClick}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="contained"
+            endIcon={<EditIcon />}
+            style={{ backgroundColor: "#1e0342", color: "white" }}
+            onClick={handleEditButtonClick}
+            disabled={selectedCandidates.length !== 1}
+          >
+            Edit
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 };
 
-return (
-  <form onSubmit={handleSendEmail}>
-    <label>
-      Recipient Email:
-      <input
-        type="email"
-        value={recipientEmail}
-        onChange={(e) => setRecipientEmail(e.target.value)}
-        required
-      />
-    </label>
-    <label>
-      Subject:
-      <input
-        type="text"
-        value={emailSubject}
-        onChange={(e) => setEmailSubject(e.target.value)}
-        required
-      />
-    </label>
-    <label>
-      Body:
-      <textarea
-        value={emailBody}
-        onChange={(e) => setEmailBody(e.target.value)}
-        required
-      />
-    </label>
-    <button type="submit">Send Email</button>
-  </form>
-);
+export default Schedule;
